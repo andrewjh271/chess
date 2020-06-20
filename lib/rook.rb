02b63eca04cf3b2f89a63a8e888e0aa975dded7a
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-06-19 18:01:49 -0700

    Sets valid moves and valid captures for Rook

--- lib/rook.rb
+++ lib/rook.rb
@@ -3,13 +3,14 @@ require_relative 'piece'
 class Rook < Piece
   private
 
-  attr_reader :has_moved
+  attr_reader :has_moved, :directions
 
   public
 
-  def self.moves(arr)
-    file = arr[0]
-    rank = arr[1]
+  # not needed  (I think...)
+  def all_moves
+    file = location[0]
+    rank = location[1]
     moves = []
     (-7..7).each do |i|
       next if i.zero?
@@ -20,9 +21,58 @@ class Rook < Piece
     moves
   end
 
+  def in_bounds_moves
+    all_moves.select { |move| move[0].between?(0, Board::MAX) && move[1].between?(0, Board::MAX) }
+  end
+  # keeping for now ^
+
+  def set_valid_moves
+    # non-captures
+    file = location[0]
+    rank = location[1]
+    directions.each do |direction|
+      new_file = file + direction[0]
+      new_rank = rank + direction[1]
+      while new_file.between?(0, Board::MAX) && 
+            new_rank.between?(0, Board::MAX) &&
+            board.squares[new_file][new_rank].nil?
+        @valid_moves << [new_file, new_rank]
+        new_file += direction[0]
+        new_rank += direction[1]
+      end
+    end
+  end
+
+  def set_valid_captures
+    # does not safeguard against capturing opponents king b/c it cannot be in check
+    file = location[0]
+    rank = location[1]
+    directions.each do |direction|
+      new_file = file + direction[0]
+      new_rank = rank + direction[1]
+      while new_file.between?(0, Board::MAX) && 
+            new_rank.between?(0, Board::MAX) &&
+            board.squares[new_file][new_rank].nil?
+        new_file += direction[0]
+        new_rank += direction[1]
+      end
+      if new_file.between?(0, Board::MAX) && 
+         new_rank.between?(0, Board::MAX) &&
+         board.squares[new_file][new_rank].white? != white?
+        valid_captures << [new_file, new_rank]
+      end
+    end
+  end
+
+
+
+
+
+
   def initialize(board, location, color)
     super
     @has_moved = false
+    @directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
   end
 
   def to_s
--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -1,14 +1,61 @@
 require './lib/rook.rb'
+require './lib/board.rb'
 
 describe Rook do
-  describe '.moves' do
-    it 'returns all possible moves from a given set of coordinates' do
-      expect(Rook.moves([0, 3])).to contain_exactly(
+  attr_reader :board
+
+  before { @board = Board.new }
+
+  describe '#all_moves' do
+    it 'returns all possible moves from its location (including moves that go off board)' do
+      rook = Rook.new(board, [0, 3], 'white')
+      expect(rook.all_moves).to contain_exactly(
         [0, -4], [0, -3], [0, -2], [0, -1], [0, 0], [0, 1], [0, 2],
         [0, 4], [0, 5], [0, 6], [0, 7], [0, 8], [0, 9], [0, 10],
         [-7, 3], [-6, 3], [-5, 3], [-4, 3], [-3, 3], [-2, 3], [-1, 3],
         [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3]
       )
+    end
+  end
+
+  describe '#in_bounds_moves' do
+    it 'returns all moves from its location that stay on board' do
+      rook = Rook.new(board, [1, 1], 'white')
+      expect(rook.in_bounds_moves).to contain_exactly(
+        [0, 1], [1, 2], [1, 3], [1, 4], [1, 5], [1, 6], [1, 7],
+        [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [1, 0]
+      )
+    end
+  end
+
+  describe '#set_valid_moves' do
+    it 'sets @valid_moves' do
+      rook = Rook.new(board, [3, 2], 'white')
+      rook.set_valid_moves
+      expect(rook.valid_moves).to contain_exactly(
+        [0, 2], [1, 2], [2, 2], [4, 2], [5, 2], [6, 2],
+        [7, 2], [3, 3], [3, 4], [3, 5]
+      )
+    end
+  end
+
+  describe '#set_valid_captures' do
+    it 'sets @valid_captures' do
+      rook = Rook.new(board, [7, 3], 'black')
+      rook.set_valid_captures
+      expect(rook.valid_captures).to eq([[7, 1]])
+    end
+
+    it 'sets @valid_captures' do
+      rook = Rook.new(board, [2, 2], 'black')
+      rook.set_valid_captures
+      expect(rook.valid_captures).to eq([[2, 1]])
+    end
+
+    it 'sets @valid_captures' do
+      rook = Rook.new(board, [0, 4], 'white')
+      rook.set_valid_captures
+      expect(rook.valid_captures).to eq([[0, 6]])
     end
   end
 end
