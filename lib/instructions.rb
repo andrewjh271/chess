05a472f2d6264d88c050a0714e01409decc37451
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2022-07-11 18:28:23 -0700

    Clarify tutorial instructions

--- lib/instructions.rb
+++ lib/instructions.rb
@@ -8,31 +8,31 @@ module Instructions
     instruct('Input moves using Chess Algebraic Notation.')
     return if STDIN.getch == 'q'
 
-    info = 'Only enter the target square for a move. For pawns, nothing else is needed.'
+    info = 'Only enter the target square coordinates for a move. For pawns, nothing else is needed.'
     return unless move_sequence(sample, %w[e4 e5], info)
 
-    info = 'Begin with K Q R B, or N to move a King Queen Rook Bishop or Knight, respectively.'
+    info = 'Begin with K, Q, R, B, or N to move a King, Queen, Rook, Bishop, or Knight, respectively.'
     return unless move_sequence(sample, %w[Bc4 Nf6], info)
 
-    info = 'For a capture, add an x before the target square.'
+    info = 'For a capture, add an x just before the target square.'
     return unless move_sequence(sample, %w[Nf3 Nxe4 Nc3 Nxc3], info)
 
-    info = "For a pawn capture, begin with the pawn's file."
+    info = "To capture with a pawn, begin with that pawn's file."
     return unless move_sequence(sample, ['dxc3'], info)
 
-    info = 'For kingside castling enter O-O; for queenside, O-O-O.'
+    info = 'For kingside castling, enter O-O; for queenside, O-O-O.'
     return unless move_sequence(sample, %w[f6 O-O], info)
 
     info = "For en passant, enter capture as though the opponent's pawn had only moved 1 square."
     return unless move_sequence(sample, %w[d6 b4 Bg4 b5 c5 bxc6], info)
 
-    info = 'Specify the file or rank if there are more than one piece that could make move.'
+    info = 'Specify the current file or rank of the piece if more than one of that type could make the desired move.'
     return unless move_sequence(sample, %w[Qe7 Ba3 Na6 Qxd6 g5 Rad1], info)
 
-    info = 'To promote a pawn, add =Q =R =B or =N after target square.'
+    info = 'To promote a pawn, add =Q, =R, =B, or =N after the target square.'
     return unless move_sequence(sample, %w[Bh6 cxb7 f5 bxa8=R+], info)
 
-    info = 'Optionally add + or # for check or checkmate. (It will be present in move list regardless.)'
+    info = 'Optionally add + or # to indicate check or checkmate respectively. (It will be present in the move list regardless.)'
     return unless move_sequence(sample, %w[Qd8 Qxd8#], info)
 
     sample.display
